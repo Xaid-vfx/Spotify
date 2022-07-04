6f01b00d8ea677afeb02df0519ed8354560e5b32
Xaid-vfx <mohd.zaid.123786@gmail.com>
2022-07-04 21:48:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+
+let songindex = 0;
+let audioEle = new Audio('Let Me.mp3');
+let masterplay = document.getElementById('masterplay');
+let myprgbar = document.getElementById('progbar');
+let smallplay = document.getElementById('timest');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+let prev = document.getElementById('previoussong');
+let forw = document.getElementById('nextsong');
+
+let songs = [
+    { songName: "Let Me", filePath: "Let Me.mp3", coverPath: "cover.jpg" },
+    { songName: "Satisfaction", filePath: "Satisfaction.mp3", coverPath: "cover.jpg" },
+    { songName: "Tonight", filePath: "Tonight.mp3", coverPath: "cover.jpg" },
+    { songName: "Rainberry", filePath: "Rainberry.mp3", coverPath: "cover.jpg" }
+]
+
+document.getElementById("coverimg").src = "cover.jpg";
+let a = document.getElementsByClassName("songName");
+for (i = 0; i < songs.length; i++) {
+    a[i].innerHTML = songs[i].songName;
+}
+
+masterplay.addEventListener('click', () => {
+    if (audioEle.paused || audioEle.currentTime <= 0) {
+        audioEle.play();
+        masterplay.classList.remove("fa-play-circle")
+        masterplay.classList.add("fa-pause-circle")
+        document.getElementById("gif").style.opacity = 1;
+    }
+    else {
+        audioEle.pause();
+        masterplay.classList.replace("fa-pause-circle", "fa-play-circle")
+        document.getElementById("gif").style.opacity = 0;
+    }
+    //    alert('play');
+    setInterval(function () {
+        if (audioEle.duration == audioEle.currentTime) {
+            if (songindex == songs.length - 1)
+                songindex = 0;
+            else
+                songindex += 1;
+            audioEle.pause();
+            audioEle.src = songs[songindex].filePath;
+            update();
+            audioEle.play();
+        }
+    }, 1000)
+
+})
+
+audioEle.addEventListener('timeupdate', () => {
+    // console.log('timeupdate');
+    prog = parseInt((audioEle.currentTime / audioEle.duration) * 100);
+    // console.log(prog);
+    myprgbar.value = prog;
+})
+
+myprgbar.addEventListener('change', () => {
+    audioEle.currentTime = (myprgbar.value * audioEle.duration / 100);
+})
+
+function update() {
+    document.getElementById('detoo').innerHTML = songs[songindex].songName;
+}
+
+prev.addEventListener('click', () => {
+    if (songindex == 0)
+        songindex = songs.length - 1;
+    else
+        songindex -= 1;
+    audioEle.pause();
+    audioEle.src = songs[songindex].filePath;
+    update();
+    audioEle.play();
+    if(masterplay.classList.contains("fa-play-circle"))
+        masterplay.classList.replace("fa-play-circle", "fa-pause-circle");
+})
+
+forw.addEventListener('click', () => {
+    if (songindex == songs.length - 1)
+        songindex = 0;
+    else
+        songindex += 1;
+    audioEle.pause();
+    audioEle.src = songs[songindex].filePath;
+    update();
+    audioEle.play();
+    if(masterplay.classList.contains("fa-play-circle"))
+        masterplay.classList.replace("fa-play-circle", "fa-pause-circle");
+})
+
+
